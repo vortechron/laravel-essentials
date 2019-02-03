f42eebafdc56afe51a6190d25f65a62d745a5331
Amirul Adli <sayaamiruladli@gmail.com>
2019-02-03 12:25:57 +0800

    fix

--- config/config.php
+++ config/config.php
@@ -5,26 +5,26 @@ return [
 
     'commands' => [
         'package_json' => [
-            "@estudioliver/vue-uuid-v4" => "^1.0.0",
+            // "@estudioliver/vue-uuid-v4" => "^1.0.0",
             "bootstrap-vue" => "^2.0.0-rc.11",
-            "form-backend-validation" => "^2.3.3",
-            "moment" => "^2.22.2",
-            "moment-timezone" => "^0.5.21",
-            "slugify" => "^1.3.1",
-            "sweetalert2" => "^7.28.4",
-            "tailwindcss" => "^0.6.6",
-            "v-money" => "^0.8.1",
-            "validator" => "^10.8.0",
-            "vue-avatar" => "^2.1.6",
-            "vue-element-loading" => "^1.0.4",
-            "vue-flatpickr-component" => "^7.0.6",
-            "vue-form-generator" => "^2.3.1",
-            "vue-multiselect" => "^2.1.3",
-            "vue-overdrive" => "0.0.12",
-            "vue-social-sharing" => "^2.3.3",
-            "vue-wait" => "^1.3.2",
-            "vue2-transitions" => "^0.2.3",
-            "vuedraggable" => "^2.16.0"
+            // "form-backend-validation" => "^2.3.3",
+            // "moment" => "^2.22.2",
+            // "moment-timezone" => "^0.5.21",
+            // "slugify" => "^1.3.1",
+            // "sweetalert2" => "^7.28.4",
+            // "tailwindcss" => "^0.6.6",
+            // "v-money" => "^0.8.1",
+            // "validator" => "^10.8.0",
+            // "vue-avatar" => "^2.1.6",
+            // "vue-element-loading" => "^1.0.4",
+            // "vue-flatpickr-component" => "^7.0.6",
+            // "vue-form-generator" => "^2.3.1",
+            // "vue-multiselect" => "^2.1.3",
+            // "vue-overdrive" => "0.0.12",
+            // "vue-social-sharing" => "^2.3.3",
+            // "vue-wait" => "^1.3.2",
+            // "vue2-transitions" => "^0.2.3",
+            // "vuedraggable" => "^2.16.0"
         ]
     ]
 ];
