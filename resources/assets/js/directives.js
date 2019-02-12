6674a4889fdf133ed9f94dde058d7b7a88d7cd44
Amirul Adli <sayaamiruladli@gmail.com>
2019-02-12 18:28:55 +0800

    Update directives.js

--- resources/assets/js/directives.js
+++ resources/assets/js/directives.js
@@ -1,10 +1,18 @@
 // v-post on any anchor element to give the capability to post request to given route
 Vue.directive('post', {
     bind: function (el, binding, vnode) {
-        var id = Math.random().toString(36).substring(7);
-      el.setAttribute('onclick', `event.preventDefault();document.getElementById(\'${id}\').submit();`);
-        
-      var form = `<form id="${id}" action="${binding.value}" method="POST" style="display: none;">
+
+      var id = Math.random().toString(36).substring(7);
+      var route = binding.value;
+      var attrib = `event.preventDefault();document.getElementById(\'${id}\').submit();`;
+
+      if (binding.arg == 'confirm') {
+        route = binding.value.route;
+        attrib = `event.preventDefault();if(confirm('${binding.value.confirm_msg}')) {document.getElementById(\'${id}\').submit();}`
+      }
+
+      el.setAttribute('onclick', attrib);
+      var form = `<form id="${id}" action="${route}" method="POST" style="display: none;">
       <input type="hidden" name="_token" value="${document.head.querySelector('meta[name="csrf-token"]').content}">
         </form>`
       el.innerHTML = el.textContent + form;
