86b15f5d823eb818dbce98e2bd32f84342a6f466
Vortechron <sayaamiruladli@gmail.com>
2019-05-16 20:46:25 +0800

    Update alerts.blade.php

--- resources/views/supports/alerts.blade.php
+++ resources/views/supports/alerts.blade.php
@@ -1,5 +1,5 @@
 @if ($flash = session('successFlash'))
-<div class="bg-green-lightest border-t-4 border-green rounded-b text-green-darkest px-4 py-3 shadow-md" role="alert">
+<div class="mb-4 bg-green-lightest border-t-4 border-green rounded-b text-green-darkest px-4 py-3 shadow-md" role="alert">
         <div>
             <p class="font-bold">{!! $flash['title'] !!}</p>
         </div>
@@ -7,7 +7,7 @@
 @endif
 
 @if ($flash = session('warningFlash'))
-<div class="bg-orange-lightest border-t-4 border-orange rounded-b text-orange-darkest px-4 py-3 shadow-md" role="alert">
+<div class="mb-4 bg-orange-lightest border-t-4 border-orange rounded-b text-orange-darkest px-4 py-3 shadow-md" role="alert">
         <div>
             <p class="font-bold">{!! $flash['title'] !!}</p>
         </div>
@@ -15,7 +15,7 @@
 @endif
 
 @if ($flash = session('dangerFlash'))
-<div class="bg-red-lightest border-t-4 border-red rounded-b text-red-darkest px-4 py-3 shadow-md" role="alert">
+<div class="mb-4 bg-red-lightest border-t-4 border-red rounded-b text-red-darkest px-4 py-3 shadow-md" role="alert">
         <div>
             <p class="font-bold">{!! $flash['title'] !!}</p>
         </div>
