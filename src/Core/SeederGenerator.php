d63cbc205b6b341c4ede9e5928328be6c84b7e3d
vortechron <sayaamiruladli@gmail.com>
2020-06-08 01:52:35 +0800

    wip

--- src/Core/SeederGenerator.php
+++ src/Core/SeederGenerator.php
@@ -9,21 +9,18 @@ class SeederGenerator
 {
     public static function mapPermissions($permissions, $roles)
     {
-        Role::truncate();
-        Permission::truncate();
-
         foreach ($roles as $roleName) {
             $role = Role::whereName($roleName)->first();
 
             if (! $role) $role = Role::create(['name' => $roleName]);
 
             foreach ($permissions as $permissionName) {
-                $permission = Permission::whereName('manage '. $permissionName)->first();
+                $permission = Permission::whereName($permissionName)->first();
 
-                if (! $permission) $permission = Permission::create(['name' => 'manage '. $permissionName]);
+                if (! $permission) $permission = Permission::create(['name' => $permissionName]);
                 
-                if ($permission && ! $role->hasPermissionTo('manage '. $permissionName)) {
-                    $role->givePermissionTo('manage '. $permissionName);
+                if ($permission && ! $role->hasPermissionTo($permissionName)) {
+                    $role->givePermissionTo($permissionName);
                 }
             }
         }
